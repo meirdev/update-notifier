aacbe7c42262d3245602c2c89f0ef1b3c5ddb10e
meirdev <maornet@gmail.com>
2022-10-24 15:29:58 +0300

    replace string with var

--- example.py
+++ example.py
@@ -8,7 +8,7 @@ __version__ = "0.1.0"
 update_notifier_mytest = update_notifier(
     name=__title__,
     current_version=__version__,
-    latest_version=PyPi("requests"),
+    latest_version=PyPi(__title__),
 )
 
 update_notifier_mytest.notify(Poetry())
